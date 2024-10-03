60e26c1991fbc0dcc376ba87349a047d8c445f11
alesan99 <alesanesp99@gmail.com>
2024-10-03 11:44:08 -0500

    Add input validation

--- lab04/code2.c
+++ lab04/code2.c
@@ -4,53 +4,87 @@ float celsius_to_fahrenheit (float celsius);
 float fahrenheit_to_celsius (float fahrenheit);
 float celsius_to_kelvin (float celsius);
 float kelvin_to_celsius (float kelvin);
+float fahrenheit_to_kelvin (float fahrenheit);
+float kelvin_to_fahrenheit (float kelvin);
 void categorize_temperature(float celsius);
 
 int main() {
-	float temperature;
-	float convertedTemperature;
-	float celciusTemperature;
-	int inputUnit;
-	int outputUnit;
+	float temperature; // input temperature
+	float convertedTemperature; // output temperature
+	float celciusTemperature; // celcius temperature specifically, needed for categorization
+	int inputScale; // 1 = Celsius, 2 = Fahrenheit, 3 = Kelvin
+	int outputScale; // same as above
 
-	printf("Enter the temperature: ");
-	scanf("%f", &temperature);
-	printf("Choose the current scale (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
-	scanf("%d", &inputUnit);
-	printf("Convert to (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
-	scanf("%d", &outputUnit);
-	printf("\n");
+	// Prompt user for input
+	while (1) {
+		printf("Enter the temperature: ");
+		scanf("%f", &temperature);
+		printf("Choose the current scale (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
+		scanf("%d", &inputScale);
+		printf("Convert to (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
+		scanf("%d", &outputScale);
+		printf("\n");
 
-	if (inputUnit == 1) {
-		if (outputUnit == 1) {
-			convertedTemperature = temperature;
-		} else if (outputUnit == 2) {
+		// Validate inputs
+		// no negative kelvin
+		if (inputScale == 3 && temperature < 0) {
+			printf("Invalid temperature for Kelvin. Must be a positive value.\n");
+			continue; // try again
+		}
+		// must be a valid scale
+		if (inputScale < 1 || inputScale > 3) {
+			printf("Invalid current scale\n");
+			continue; // try again
+		}
+		if (outputScale < 1 || outputScale > 3) {
+			printf("Invalid target scale\n");
+			continue; // try again
+		}
+		// must be a valid conversion
+		if (inputScale == outputScale) {
+			printf("No conversion needed\n");
+			continue; // try again
+		}
+
+		break;
+	}
+
+	// Handle all conversion cases
+	convertedTemperature = temperature; // assume no conversion is neccessary by default
+	if (inputScale == 1) {
+		// Convert from celcius
+		if (outputScale == 2) {
 			convertedTemperature = celsius_to_fahrenheit(temperature);
-		} else if (outputUnit == 3) {
+		} else if (outputScale == 3) {
 			convertedTemperature = celsius_to_kelvin(temperature);
 		}
 		celciusTemperature = temperature;
-	} else if (inputUnit == 2) {
-		if (outputUnit == 1) {
+	} else if (inputScale == 2) {
+		// Convert from fahrenheit
+		if (outputScale == 1) {
 			convertedTemperature = fahrenheit_to_celsius(temperature);
-		} else if (outputUnit == 2) {
-			convertedTemperature = temperature;
-		} else if (outputUnit == 3) {
-			convertedTemperature = celsius_to_kelvin(fahrenheit_to_celsius(temperature));
+		} else if (outputScale == 3) {
+			convertedTemperature = fahrenheit_to_kelvin(temperature);
 		}
 		celciusTemperature = fahrenheit_to_celsius(temperature);
-	} else if (inputUnit == 3) {
-		if (outputUnit == 1) {
+	} else if (inputScale == 3) {
+		// Convert from kelvin
+		if (outputScale == 1) {
 			convertedTemperature = kelvin_to_celsius(temperature);
-		} else if (outputUnit == 2) {
-			convertedTemperature = celsius_to_fahrenheit(kelvin_to_celsius(temperature));
-		} else if (outputUnit == 3) {
-			convertedTemperature = temperature;
+		} else if (outputScale == 2) {
+			convertedTemperature = kelvin_to_fahrenheit(temperature);
 		}
 		celciusTemperature = kelvin_to_celsius(temperature);
 	}
 
-	printf("Converted temperature: %.2f\n", convertedTemperature);
+	// Print the converted temperature
+	if (outputScale == 1) {
+		printf("Converted temperature: %.2f°C\n", convertedTemperature);
+	} else if (outputScale == 2) {
+		printf("Converted temperature: %.2f°F\n", convertedTemperature);
+	} else if (outputScale == 3) {
+		printf("Converted temperature: %.2f K\n", convertedTemperature);
+	}
 
 	// Categorize
 	categorize_temperature(celciusTemperature);
@@ -74,7 +108,16 @@ float kelvin_to_celsius (float kelvin) {
 	return kelvin - 273.15;
 }
 
+float fahrenheit_to_kelvin (float fahrenheit) {
+	return celsius_to_kelvin(fahrenheit_to_celsius(fahrenheit));
+}
+
+float kelvin_to_fahrenheit (float kelvin) {
+	return celsius_to_fahrenheit(kelvin_to_celsius(kelvin));
+}
+
 void categorize_temperature(float celsius) {
+	// Describe temperature and provide an advisory
 	if (celsius < 0) {
 		printf("Temperature category: Feezing\n");
 		printf("Weather advisory: Stay indoors\n");
@@ -86,9 +129,9 @@ void categorize_temperature(float celsius) {
 		printf("Weather advisory: You should feel comfortable.\n");
 	} else if (celsius < 35) {
 		printf("Temperature category: Hot\n");
-		printf("Weather advisory: Wear light clothing\n");
+		printf("Weather advisory: Dress lightly\n");
 	} else {
 		printf("Temperature category: Extreme Heat\n");
-		printf("Weather advisory: Stay hydrated\n");
+		printf("Weather advisory: Excercise caution, stay hydrated\n");
 	}
 }
